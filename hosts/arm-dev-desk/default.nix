3a6f1282287ad3eaf51e410eea0f359eb15aa3ff
Aidan De Angelis <angaidan@dev-dsk-angaidan-2b-8ba1a9f5.us-west-2.amazon.com>
2024-10-05 00:33:05 +0000

    Install stylua on dev-desks

--- hosts/arm-dev-desk/default.nix
+++ hosts/arm-dev-desk/default.nix
@@ -12,6 +12,7 @@
   home = {
     packages = with pkgs; [
       (lib.hiPrio opensshWithKerberos)
+      stylua
     ];
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
