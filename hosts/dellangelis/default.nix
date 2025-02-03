562568f3f03b9191063e1000d2147ae9b8f17af0
Aidan De Angelis <aidandeangelis@berkeley.edu>
2025-02-02 19:10:30 -0800

    add tor and mullvad browsers to dellangelis

--- hosts/dellangelis/default.nix
+++ hosts/dellangelis/default.nix
@@ -25,6 +25,9 @@
 
       obsidian
       vlc
+
+      mullvad-browser
+      tor-browser
     ];
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
