4f9bfd079bef76bb0a08e380cd4f26cdbfd1be93
Aidan De Angelis <angaidan@amazon.com>
2025-02-14 09:28:31 -0800

    tmux rebinds

--- common/tmux.nix
+++ common/tmux.nix
@@ -225,10 +225,10 @@ in
 
 
           # -- Window --
-          bind p previous-window
           bind C-p previous-window
-          bind n next-window
+          bind C-b previous-window
           bind C-n next-window
+          bind C-a next-window
 
           # -- Pane --
           unbind %
