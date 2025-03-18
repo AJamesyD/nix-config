b9241c69b5eecb4ebbbc0cc770d260b9a65c9b99
Aidan De Angelis <angaidan@amazon.com>
2025-03-18 09:21:25 -0700

    aerospace updates for firefox

--- hosts/m3-work-laptop/aerospace.nix
+++ hosts/m3-work-laptop/aerospace.nix
@@ -58,45 +58,23 @@
         cmd-shift-alt-ctrl-k = "move-node-to-monitor up --focus-follows-window";
         cmd-shift-alt-ctrl-l = "move-node-to-monitor right --focus-follows-window";
 
-        /*
-          Workspace mapping
-          1: Dev (sometimes also Firefox)
-          2: Firefox
-          3: Slack
-          4: Chime
-          5: Admin (Obsidian + Outlook)
-          6: Miscellaneous (e.g. Spotify)
-          _: Ignored
-        */
-        cmd-shift-alt-1 = "workspace dev";
-        cmd-shift-alt-2 = "workspace firefox";
-        cmd-shift-alt-3 = "workspace slack";
-        cmd-shift-alt-4 = "workspace chime";
-        cmd-shift-alt-5 = "workspace admin";
-        cmd-shift-alt-6 = "workspace misc";
-        cmd-shift-alt-7 = "workspace 7";
-        cmd-shift-alt-8 = "workspace 8";
-        cmd-shift-alt-9 = "workspace 9";
         cmd-shift-alt-a = "workspace admin";
         cmd-shift-alt-d = "workspace dev";
         cmd-shift-alt-c = "workspace chime";
         cmd-shift-alt-f = "workspace firefox";
+        cmd-shift-alt-1 = "workspace firefox";
+        cmd-shift-alt-2 = "workspace firefox-2";
+        cmd-shift-alt-3 = "workspace firefox-3";
         cmd-shift-alt-m = "workspace misc";
         cmd-shift-alt-s = "workspace slack";
 
-        cmd-shift-alt-ctrl-1 = "move-node-to-workspace dev --focus-follows-window";
-        cmd-shift-alt-ctrl-2 = "move-node-to-workspace firefox --focus-follows-window";
-        cmd-shift-alt-ctrl-3 = "move-node-to-workspace slack --focus-follows-window";
-        cmd-shift-alt-ctrl-4 = "move-node-to-workspace chime --focus-follows-window";
-        cmd-shift-alt-ctrl-5 = "move-node-to-workspace admin --focus-follows-window";
-        cmd-shift-alt-ctrl-6 = "move-node-to-workspace misc --focus-follows-window";
-        cmd-shift-alt-ctrl-7 = "move-node-to-workspace 7 --focus-follows-window";
-        cmd-shift-alt-ctrl-8 = "move-node-to-workspace 8 --focus-follows-window";
-        cmd-shift-alt-ctrl-9 = "move-node-to-workspace 9 --focus-follows-window";
         cmd-shift-alt-ctrl-a = "move-node-to-workspace admin --focus-follows-window";
         cmd-shift-alt-ctrl-c = "move-node-to-workspace chime --focus-follows-window";
         cmd-shift-alt-ctrl-d = "move-node-to-workspace dev --focus-follows-window";
         cmd-shift-alt-ctrl-f = "move-node-to-workspace firefox --focus-follows-window";
+        cmd-shift-alt-ctrl-1 = "move-node-to-workspace firefox --focus-follows-window";
+        cmd-shift-alt-ctrl-2 = "move-node-to-workspace firefox-2 --focus-follows-window";
+        cmd-shift-alt-ctrl-3 = "move-node-to-workspace firefox-3 --focus-follows-window";
         cmd-shift-alt-ctrl-m = "move-node-to-workspace misc --focus-follows-window";
         cmd-shift-alt-ctrl-s = "move-node-to-workspace slack --focus-follows-window";
 
@@ -228,6 +206,15 @@
       "secondary"
     ];
     firefox = "built-in";
+    firefox-2 = [
+      "p27"
+      "dell"
+      "secondary"
+    ];
+    firefox-3 = [
+      "p32"
+      "secondary"
+    ];
     ignored = [
       "p32"
       "secondary"
