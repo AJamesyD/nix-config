d1bce696561f43ce876847f01909b3952d6775ce
Aidan De Angelis <angaidan@amazon.com>
2025-08-13 18:57:43 +0000

    shpool config

--- hosts/al2-x86-cdd/default.nix
+++ hosts/al2-x86-cdd/default.nix
@@ -61,11 +61,53 @@
             zsource
           '';
         up = "sudo yum upgrade -y && nixup";
+        # Borrowed from https://github.com/shell-pool/shpool/issues/49#issue-2355077641
+        shll = # bash
+          ''
+            # shpool session selector
+            # keybindings:
+            #     k/x to kill
+            #     a/n/enter to attach
+            # Shortcut: ctrl+a then w
+            shpool_choose() {
+            	cmd_output=$(
+            		shpool list | tail -n +2 | cut -f1 | fzf \
+            			--bind 'k:execute(shpool kill {})' \
+            			--bind 'x:execute(shpool kill {})' \
+            			--bind 'a:execute(shpool attach --force {})' \
+            			--bind 'n:execute(shpool attach --force {})' \
+            			--preview 'shpool list | tail -n +2 | sed -n "$(({n}+1))"p' \
+            			--bind "change:reload(shpool list | tail -n +2)" \
+            			--reverse \
+            			--height=~100% \
+            			--preview-window down:wrap \
+            			--header "Shpool sessions" \
+            			--no-select-1 \
+            			--no-exit-0
+            	)
+
+            	# notify-send "$cmd_output"
+            	[ -n "$cmd_output" ] && shpool attach --force "$cmd_output"
+            }
+
+            shpool_choose
+          '';
       };
     };
   };
 
-  services.shpool.enable = true;
+  services.shpool = {
+    enable = true;
+    settings = {
+      session_restore_mode = "screen";
+      keybinding = [
+        {
+          binding = "Ctrl-a Ctrl-q";
+          action = "detach";
+        }
+      ];
+    };
+  };
 
   targets.genericLinux.enable = true;
 }
