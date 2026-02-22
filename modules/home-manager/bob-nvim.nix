1ffd0b18cd0e76634822024545b2f2cb085aac26
Aidan De Angelis <angaidan@amazon.com>
2026-02-21 19:34:16 -0800

    chore(bob-nvim): update to v4.1.6
    Latest release (2025-12-06) includes fixes for installation via
    commit/hash and dependency updates.

--- modules/home-manager/bob-nvim.nix
+++ modules/home-manager/bob-nvim.nix
@@ -22,16 +22,16 @@ in
     home.packages = with pkgs; [
       (rustPlatform.buildRustPackage {
         pname = "bob-nvim";
-        version = "4.1.2";
+        version = "4.1.6";
 
         src = fetchFromGitHub {
           owner = "MordechaiHadad";
           repo = "bob";
-          rev = "v4.1.2";
-          sha256 = "sha256-l4WfMvRPvnqra4jiK35w2SwfgxeRCYLl3ZWtD2UTQqw=";
+          rev = "v4.1.6";
+          sha256 = "sha256-XI/oNGKLXQ/fpB6MojhTsEgmmPH1pHECD5oZgc1r4rQ=";
         };
 
-        cargoHash = "sha256-bcDCbAOFth3/o+USoJY1NPZyPWyZA3jhruRMnPo8kOQ=";
+        cargoHash = "sha256-YSZcYTGnMnN/srh8Z15toq+GIyRKfFd+pGkFQl5gCuo=";
         doCheck = false;
       })
     ];
