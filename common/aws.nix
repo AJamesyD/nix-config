23d3289d5f7f5471ab36d3cba75e6b6124661639
Aidan De Angelis <aidandeangelis@berkeley.edu>
2026-04-07 15:39:13 -0700

    feat(shell): add brazil workspace context detection via chpwd hook
    Walk up from CWD to find `packageInfo` (workspace root) and
    case-sensitive `Config` (package root). Export `_BRAZIL_WS` (dir
    name), `_BRAZIL_PKG`, and `_BRAZIL_PKG_VER` (from interfaces).

--- common/aws.nix
+++ common/aws.nix
@@ -146,6 +146,34 @@ in
             fi
           ''
         )
+        # bash
+        ''
+          _brazil_context_hook() {
+            local dir=$PWD
+            while [[ $dir != / ]]; do
+              if [[ -f "$dir/packageInfo" ]]; then
+                export _BRAZIL_WS=''${dir:t}
+                _BRAZIL_PKG=
+                _BRAZIL_PKG_VER=
+                local d=$PWD
+                while [[ $d != "$dir" && $d != / ]]; do
+                  if [[ -f "$d/Config" ]]; then
+                    export _BRAZIL_PKG=''${d:t}
+                    export _BRAZIL_PKG_VER=$(awk -F'[();]' '/interfaces/{gsub(/ /,"",$2); print $2}' "$d/Config")
+                    break
+                  fi
+                  d=''${d:h}
+                done
+                export _BRAZIL_PKG _BRAZIL_PKG_VER
+                return
+              fi
+              dir=''${dir:h}
+            done
+            unset _BRAZIL_WS _BRAZIL_PKG _BRAZIL_PKG_VER
+          }
+          add-zsh-hook chpwd _brazil_context_hook
+          _brazil_context_hook
+        ''
       ];
       sessionVariables = {
         # From default .zshrc written by `brazil setup completion`
