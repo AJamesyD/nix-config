690f8585ee27b13ad9c4f1c414e403d6dfb9db68
Aidan De Angelis <aidandeangelis@berkeley.edu>
2026-05-13 08:59:34 -0700

    feat: revert PI_CODING_AGENT_DIR due to inconsistent enforcement

--- common/ai/pi.nix
+++ common/ai/pi.nix
@@ -23,7 +23,6 @@ in
   ];
 
   home.sessionVariables = {
-    PI_CODING_AGENT_DIR = "${config.xdg.configHome}/pi";
     PI_SKIP_VERSION_CHECK = "1";
     PI_TELEMETRY = "0";
     PI_CACHE_RETENTION = "long";
